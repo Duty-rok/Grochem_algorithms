750bad7347ac8276cc3242ab82694f3889b510b9
Александр Шашкин <sasha.shashkin51@gmail.com>
2020-08-06 14:02:24 +0300

    Finished serach in width I have refined class Queue also

---
+++ searchInWidth/Dequeue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace searchInWidth
+{
+    class Deque:Queue
+    {
+        public Deque() { }
+        public Deque(params object[] massive)
+        {
+            foreach (object obj in massive)
+                Enqueue(obj);
+        }
+        public static Deque operator +(Deque deq1, Deque deq2)
+        {
+            Deque resDeq = new Deque();
+            foreach (object obj in deq1)
+                resDeq.Enqueue(obj);
+            foreach (object obj in deq2)
+                resDeq.Enqueue(obj);
+            return resDeq;
+        }
+    }
+}
--- searchInWidth/Program.cs
+++ searchInWidth/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace searchInWidth
 {
@@ -10,6 +8,47 @@ namespace searchInWidth
     {
         static void Main(string[] args)
         {
+            Hashtable graph = new Hashtable();
+            graph.Add("you", new Deque("alice", "bob", "claire"));
+            graph.Add("bob", new Deque("anuj", "peggy"));
+            graph.Add("alice", new Deque("peggy"));
+            graph.Add("claire", new Deque("thom", "jonny"));
+            graph.Add("anuj", new Deque());
+            graph.Add("peggy", new Deque());
+            graph.Add("thom", new Deque());
+            graph.Add("jonny", new Deque());
+            if (!search(graph, "you"))
+                WriteLine("We can't find a mango seller");
+            ReadKey(true);
+        }
+        static bool personIsSeller(string name)
+        {
+            return name[name.Length - 1] == 'm';
+        }
+        static bool search(Hashtable graph, string startName)
+        {
+            Deque search_queue = new Deque();
+            search_queue += (Deque)graph[startName];
+            List<string> searched = new List<string>();
+            string person;
+            while (search_queue.Count != 0)
+            {
+                person = (string)search_queue.Dequeue();
+                if (!searched.Contains(person))
+                {
+                    if (personIsSeller(person))
+                    {
+                        WriteLine($"{person} is a mango seller");
+                        return true;
+                    }
+                    else
+                    {
+                        search_queue += (Deque)graph[person];
+                        searched.Add(person);
+                    }
+                }
+            }
+            return false;
         }
     }
 }
