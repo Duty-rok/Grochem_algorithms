908f1c926dffcabd7685652297206bd9d20be90f
Александр Шашкин <sasha.shashkin51@gmail.com>
2020-08-15 15:50:46 +0300

    greedy_algorithm's work is finished I have also added new class Plenty.cs, because standart c# library do not exist it.

---
+++ greedy_algorithm/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using static System.Console;
+
+namespace greedy_algorithm
+{
+    class Program
+    {
+        static Plenty<string> states_needed;
+        static Hashtable stations;
+        static Plenty<string> final_stations;
+        static void Main(string[] args)
+        {
+            states_needed = new Plenty<string>("mt", "wa", "or", "id", "nv", "ut", "ca", "az");
+            stations = new Hashtable();
+            stations.Add("kone", new Plenty<string>("id", "nv", "ut"));
+            stations.Add("ktwo", new Plenty<string>("id", "wa", "mt"));
+            stations.Add("kthree", new Plenty<string>("or", "nv", "ca"));
+            stations.Add("kfour", new Plenty<string>("nv", "ut"));
+            stations.Add("kfive", new Plenty<string>("ca", "az"));
+            final_stations = new Plenty<string>();
+            while (states_needed.Count != 0)
+            {
+                string best_station = "none";
+                Plenty<string> states_covered = new Plenty<string>();
+                foreach(object station in stations.Keys)
+                {
+                    Plenty<string> covered = states_needed * ((Plenty<string>)stations[station]);
+                    if (covered.Count > states_covered.Count)
+                    {
+                        best_station = (string)station;
+                        states_covered = covered;
+                    }
+                }
+                final_stations.Add(best_station);
+                states_needed -= states_covered;
+            }
+            foreach (string station in final_stations)
+                Write($"{station} ");
+        }
+    }
+}
---
+++ greedy_algorithm/plenty.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace greedy_algorithm
+{
+    class Plenty<T>:IEnumerable
+    {
+        protected List<T> _items = new List<T>();
+        public Plenty() { }
+
+        public Plenty(params T[] massive)
+        {
+            foreach (T obj in massive)
+                if (!_items.Contains(obj))
+                    _items.Add(obj);
+        }
+        public int Count => _items.Count;
+        public IEnumerator GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+        public static Plenty<T> operator +(Plenty<T> plnt1, Plenty<T> plnt2)
+        {
+            Plenty<T> rezult = new Plenty<T>();
+            foreach (T obj in plnt1._items)
+                if (!rezult._items.Contains(obj))
+                    rezult._items.Add(obj);
+            foreach (T obj in plnt2._items)
+                if (!rezult._items.Contains(obj))
+                    rezult._items.Add(obj);
+            return rezult;
+        }
+        public static Plenty<T> operator *(Plenty<T> plnt1, Plenty<T> plnt2)
+        {
+            Plenty<T> rezult = new Plenty<T>();
+            foreach (T obj in plnt1._items)
+                if (plnt2._items.Contains(obj))
+                    rezult._items.Add(obj);
+            return rezult;
+        }
+        public static Plenty<T> operator -(Plenty<T> plnt1, Plenty<T> plnt2)
+        {
+            Plenty<T> rezult = new Plenty<T>();
+            foreach (T obj in plnt1._items)
+                if (!plnt2._items.Contains(obj))
+                    rezult._items.Add(obj);
+            return rezult;
+        }
+        public void Add(params T[] massive)
+        {
+            foreach (T obj in massive)
+                if (!_items.Contains(obj))
+                    _items.Add(obj);
+        }
+        public void Add(T obj)
+        {
+            if (!_items.Contains(obj))
+                _items.Add(obj);
+        }
+        public void Remove(T obj)
+        {
+            if (!_items.Contains(obj))
+                throw new Exception($"PlentyNotHaveObject {obj}");
+            _items.Remove(obj);
+        }
+        public void Remove(params T[] massive)
+        {
+            foreach (T obj in massive)
+                if (!_items.Contains(obj))
+                    throw new Exception($"PlentyNotHaveObject {obj}");
+                else
+                    _items.Remove(obj);
+        }
+    }
+}
